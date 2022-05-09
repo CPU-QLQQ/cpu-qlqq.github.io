23a32af431e3bae3b8f951f42b080ff17173600f
legend <859308978@qq.com>
2022-05-09 11:45:33 +0800

    Complete the redesign and beautification of the official website

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+//用到的各个插件
+var gulp = require('gulp');
+var fontmin = require('gulp-fontmin');
+//压缩字体
+function minifyFont(text, cb) {
+	gulp
+		.src('./font/*.ttf') //原字体所在目录
+		.pipe(fontmin({
+			text: text
+		}))
+		.pipe(gulp.dest('./fontdest/')) //压缩后的输出目录
+		.on('end', cb);
+}
+
+gulp.task('minify-font', (cb) => {
+	var buffers = [];
+	gulp
+		.src(['./index.html']) //HTML文件所在目录请根据自身情况修改
+		.on('data', function(file) {
+			buffers.push(file.contents);
+		})
+		.on('end', function() {
+			var text = Buffer.concat(buffers).toString('utf-8');
+			minifyFont(text, cb);
+		});
+});
+// 运行gulp命令时依次执行以下任务
+gulp.task('default', gulp.parallel('minify-font'));
